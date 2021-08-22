b044163d889e9495c4d3634c59a86abf98c05293
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-22 16:12:41 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+a = float(input("ingresa tu calificacion de la materia 1:"))
+b = float(input("ingresa tu calificacion de la materia 2:"))
+c = float(input("ingresa tu calificacion de la materia 3:"))
+d = float(input("ingresa tu calificacion de la materia 4:"))
+cal = (a+b+c+d) / 4
+print("el promdeio de calificacion de estas 4 materias es:")
+print(cal)
 
 
 if __name__ == '__main__':
