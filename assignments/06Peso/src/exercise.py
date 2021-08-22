bcf4adaa961003d7fa2a7ec4c818cddc925d1e81
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-22 18:25:24 -0500

    Update exercise.py

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+a = int(input("Dame el peso inicial:"))
+b = int(input("Dame el peso final:"))
+c = int(input("Dame la cantidad de meses:"))
+peso = (a-b) / c
+print("Lo que debes bajar por mes es:")
+print(peso)
+
     
 
 
