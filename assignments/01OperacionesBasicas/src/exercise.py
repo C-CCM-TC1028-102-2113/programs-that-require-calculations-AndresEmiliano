3c174a59e97c05b4255816b0626c43debacb2280
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-22 15:49:46 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    a = int(input("dame un numero entero:"))
+b = int(input("dame otro numero entero"))
+c = a+b
+d = a-b
+e = a*b
+print("la suma de ambos numeros es:")
+print(c)
+print("la resta del primer numero menos el segundo es:")
+print(d)
+print("la multiplicacion de ambos numeros es:")
+print(e)
 
 
 if __name__ == '__main__':
